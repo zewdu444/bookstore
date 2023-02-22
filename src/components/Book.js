45c39c81f727465fcdd7ba3c08f00251c35bb485
zewdu erkyhun <zewdu444@gmail.com>
2023-02-22 21:08:46 +0300

    Add book display

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
-function Book() {
+function Book({ books }) {
+  const booklist = books.length ? (
+    <ul>
+      {
+      books.map((book) => (
+        <li key={book.id}>
+          {book.title}
+          <p>
+            {book.author}
+          </p>
+          <button id={book.id} type="button">Remove</button>
+        </li>
+      ))
+    }
+    </ul>
+  ) : (
+    <p>there is no book on the shelf</p>
+  );
+
   return (
-    <div>Book</div>
+    <div>
+      {booklist}
+    </div>
   );
 }
 
