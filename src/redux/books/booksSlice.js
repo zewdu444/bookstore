261943cbd748b582055ab42d612f928dea00a1eb
zewdu erkyhun <zewdu444@gmail.com>
2023-02-27 17:26:41 +0300

    Add inital state value for store

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,7 +1,24 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  bookstore: [],
+  bookstore: [{
+    item_id: 'item1',
+    title: 'The Great Gatsby',
+    author: 'John Smith',
+    category: 'Fiction',
+  },
+  {
+    item_id: 'item2',
+    title: 'Anna Karenina',
+    author: 'Leo Tolstoy',
+    category: 'Fiction',
+  },
+  {
+    item_id: 'item3',
+    title: 'The Selfish Gene',
+    author: 'Richard Dawkins',
+    category: 'Nonfiction',
+  }],
 };
 
 export const booksSlice = createSlice({
