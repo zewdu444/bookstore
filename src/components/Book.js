8529e767d38cd2f832337f668fdbbed93e413398
zewdu erkyhun <zewdu444@gmail.com>
2023-03-02 22:37:46 +0300

    Styling category

--- src/components/Book.js
+++ src/components/Book.js
@@ -6,11 +6,11 @@ import BookView from './BookView';
 function Book() {
   return (
     <div className="grid surface-200 pt-3">
-      <div className="card col-12">
+      <div className="col-12">
         <BookView />
       </div>
       <Divider className="lg:ml-8 lg:mr-8 sm:ml-2 sm:mr-4 mb-0" layout="horizontal" />
-      <div className="card col-12">
+      <div className="col-12">
         <BookForm />
       </div>
     </div>
